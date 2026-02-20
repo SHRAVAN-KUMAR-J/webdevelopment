4130f2b4f57cc4986fce160759ac864ca4a01f75
SHRAVAN-KUMAR-J <shravanjalu7034@gmail.com>
2026-02-20 22:20:33 +0530

    lkjh

---
+++ Day11/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Greeting from "./Greeting";
+
+function App() {
+  return (
+    <div>
+      <h1>React Components Example</h1>
+
+      <Greeting name="Shravan" />
+      <Greeting name="Rahul" />
+      <Greeting name="Anjali" />
+    </div>
+  );
+}
+
+export default App;
---
+++ Day11/Greeting.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Greeting(props) {
+  return <h2>Hello, {props.name} 👋</h2>;
+}
+
+export default Greeting;
