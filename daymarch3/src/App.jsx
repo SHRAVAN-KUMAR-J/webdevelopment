e3150c3819ff51ef9d3d657fc9b4550c2981b8b3
SHRAVAN-KUMAR-J <shravanjalu7034@gmail.com>
2026-03-04 17:21:57 +0530

    lk

---
+++ daymarch3/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>React Router Example</h1>
+      <nav style={{marginBottom:"20px"}}>
+        <Link to="/">Home</Link> |{""}
+        <Link to="/about">About</Link> |{""}
+        <Link to="/contact">Contact</Link>|{""}
+        <Link to="/random">Random</Link>
+        
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
